7df945c4723df8163f7e3c978df0ddf0ef914717
Brais Maneiro Sánchez <brais.maneiro@netexlearning.com>
2024-11-23 14:15:57 +0100

    feat/Add memory saver and checkpoint interruption in the graph

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from langgraph.checkpoint.memory import MemorySaver
 
 class State(BaseModel):
     input: str
-    user_feedback: str
+    user_feedback: str = Field(default='')
 
 
 # Define now our nodes
@@ -42,8 +42,25 @@ graph = builder.compile(checkpointer=memory, interrupt_before=["human_feedback"]
 graph.get_graph().draw_mermaid_png(output_file_path="graph.png")
 
 
+if __name__ == "__main__":
+    # the thread_id is like a conversation_id or session_id
+    # helps us to differentiate runs of our graph
+    thread = {"configurable": {"thread_id": 1}}
 
+    initial_input = {"input": "hello world"}
 
+    for event in graph.stream(initial_input, thread, stream_mode="values"):
+        print(event)
 
+    print(graph.get_state(thread).next)
 
+    user_input = input("Tell me how you want to update the state: ")
+
+    graph.update_state(thread, values={"user_feedback": user_input}, as_node="human_feedback")
+
+    print("--- State after update ---")
+    print(graph.get_state(thread))
+
+    for event in graph.stream(initial_input, thread, stream_mode="values"):
+        print(event)
 
