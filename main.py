f47b3d12579a5aaeba43a16c0338ab5fbfb72fa1
Brais Maneiro Sánchez <brais.maneiro@netexlearning.com>
2024-11-25 15:01:19 +0100

    add sqlite as db for memory state persistence

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from dotenv import load_dotenv
 from pydantic import BaseModel, Field
 from langgraph.graph import StateGraph, START, END
 from langgraph.checkpoint.memory import MemorySaver
+from langgraph.checkpoint.sqlite import SqliteSaver
+import sqlite3
 import dotenv
 
 load_dotenv()
@@ -39,16 +41,21 @@ builder.add_edge("step_1", "human_feedback")
 builder.add_edge("human_feedback", "step_3")
 builder.add_edge("step_3", END)
 
+# In memorySaver
 memory = MemorySaver()
 
-graph = builder.compile(checkpointer=memory, interrupt_before=["human_feedback"])
+# sqlite memorySaver
+conn = sqlite3.connect(database="checkpoints.sqlite", check_same_thread=False)
+sqlite_memory = SqliteSaver(conn)
+
+graph = builder.compile(checkpointer=sqlite_memory, interrupt_before=["human_feedback"])
 
 graph.get_graph().draw_mermaid_png(output_file_path="graph.png")
 
 if __name__ == "__main__":
     # the thread_id is like a conversation_id or session_id
     # helps us to differentiate runs of our graph
-    thread = {"configurable": {"thread_id": 1}}
+    thread = {"configurable": {"thread_id": 2}}
 
     initial_input = {"input": "hello world"}
 
@@ -64,5 +71,5 @@ if __name__ == "__main__":
     print("--- State after update ---")
     print(graph.get_state(thread))
 
-    for event in graph.stream(initial_input, thread, stream_mode="values"):
+    for event in graph.stream(None, thread, stream_mode="values"):
         print(event)
